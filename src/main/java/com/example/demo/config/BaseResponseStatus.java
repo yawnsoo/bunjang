1a8d48dbc59231e0f66517d83d4429da8670fd1b
yawnsoo <0607yeonsoo@gmail.com>
2022-09-25 17:55:44 +0900

    edit BaseResposeStatus

--- src/main/java/com/example/demo/config/BaseResponseStatus.java
+++ src/main/java/com/example/demo/config/BaseResponseStatus.java
@@ -21,6 +21,7 @@ public enum BaseResponseStatus {
     EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
+    QUERY_STRING_ERROR(false,2004,"Parameter값을 확인해 주세요."),
 
     // users
     USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
