8c01dea36b474f4daa215412047568d38f4bb50b
yawnsoo <0607yeonsoo@gmail.com>
2022-09-26 04:08:28 +0900

    +getBrands +getBrandPosts

--- src/main/java/com/example/demo/src/home/HomeController.java
+++ src/main/java/com/example/demo/src/home/HomeController.java
@@ -3,6 +3,8 @@ package com.example.demo.src.home;
 
 import com.example.demo.config.BaseException;
 import com.example.demo.config.BaseResponse;
+import com.example.demo.src.home.model.GetBrandPostsRes;
+import com.example.demo.src.home.model.GetBrandsRes;
 import com.example.demo.src.home.model.GetHomePostsRes;
 import com.example.demo.utils.JwtService;
 import org.slf4j.Logger;
@@ -58,7 +60,76 @@ public class HomeController {
                 exception.printStackTrace();
                 return new BaseResponse<>(exception.getStatus());
             }
-
     }
+
+
+    /*
+     * 홈 브랜드 목록 조회 API
+     * [Get] /:user_id/brands
+     * @return BaseResponse<List<GetBrandsRes>>
+     * */
+    @ResponseBody
+    @GetMapping("/{user_id}/brands")
+    public BaseResponse<List<GetBrandsRes>> getBrands (@PathVariable("user_id") int userId) {
+
+        try {
+
+            int userIdxByJwt = jwtService.getUserIdx();
+            //userIdx와 접근한 유저가 같은지 확인
+            if (userId != userIdxByJwt) {
+                return new BaseResponse<>(INVALID_USER_JWT);
+            }
+
+            List<GetBrandsRes> getBrandsRes = homeProvider.getBrands(userId);
+            return new BaseResponse<>(getBrandsRes);
+
+
+        }catch (BaseException exception){
+            exception.printStackTrace();
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+    /*
+     * 홈 브랜드 추가 API
+     * [Post] /:user_id/brands
+     * @return BaseResponse<List<GetBrandsRes>>
+     * */
+
+
+
+
+
+    /*
+     * 홈 브랜드별 판매글 조회 API
+     * [Get] /:user_id/brands/:brand_id
+     * @return BaseResponse<GetBrandPostsRes>
+     * */
+    @ResponseBody
+    @GetMapping("/{user_id}/brandposts")
+    public BaseResponse<List<GetBrandPostsRes>> getBrandPosts (@PathVariable("user_id") int userId) {
+
+        try {
+
+            int userIdxByJwt = jwtService.getUserIdx();
+            //userIdx와 접근한 유저가 같은지 확인
+            if (userId != userIdxByJwt) {
+                return new BaseResponse<>(INVALID_USER_JWT);
+            }
+
+            List<GetBrandPostsRes> getBrandPostsRes = homeProvider.getBrandPosts(userId);
+            return new BaseResponse<>(getBrandPostsRes);
+
+
+        }catch (BaseException exception){
+            exception.printStackTrace();
+            return new BaseResponse<>(exception.getStatus());
+        }
+    }
+
+
+
+
+
 
 }
--- src/main/java/com/example/demo/src/home/HomeDao.java
+++ src/main/java/com/example/demo/src/home/HomeDao.java
@@ -1,5 +1,6 @@
 package com.example.demo.src.home;
 
+import com.example.demo.src.home.model.GetBrandsRes;
 import com.example.demo.src.home.model.GetHomePostsRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -55,5 +56,66 @@ public class HomeDao {
                         rs.getInt("jjim")
                 ), getHomePostsParam);
     }
+
+
+    public List<GetBrandsRes> getBrands(int userId) {
+        String getBrandsQuery = "select b.brand_id, name_kr, name_eng, bf.status\n" +
+                "from brand b \n" +
+                "    left outer join (select * from brand_follow where user_id = ?) bf on b.brand_id = bf.brand_id";
+        int getBrandsParam = userId;
+        return this.jdbcTemplate.query(getBrandsQuery,
+                (rs, rowNum) -> new GetBrandsRes(
+                        rs.getInt("brand_id"),
+                        rs.getString("name_kr"),
+                        rs.getString("name_eng"),
+                        rs.getInt("status")
+                ),getBrandsParam);
+    }
+
+    public List<GetBrandsRes> getBrand(int userId) {
+        String getBrandQuery = "select b.brand_id, name_kr, name_eng, bf.status\n" +
+                "from brand b \n" +
+                "    left outer join (select * from brand_follow where user_id = ?) bf on b.brand_id = bf.brand_id\n" +
+                "where bf.status = 1";
+        int getBrandParam = userId;
+        return this.jdbcTemplate.query(getBrandQuery,
+                (rs, rowNum) -> new GetBrandsRes(
+                        rs.getInt("brand_id"),
+                        rs.getString("name_kr"),
+                        rs.getString("name_eng"),
+                        rs.getInt("status")
+                ),getBrandParam);
+    }
+
+    public List<GetHomePostsRes> getPosts(String brandKor, String brandEng) {
+        String getPostsQuery = "select p.post_id, image_path, price, title, safepay, region,\n" +
+                "       case when 24 >= timestampdiff(HOUR, p.created_at, current_time)\n" +
+                "                then concat(timestampdiff(HOUR, p.created_at, current_time),'시간 전')\n" +
+                "                else concat(timestampdiff(DAY, p.created_at, current_time),'일 전') end created_at,\n" +
+                "       count(distinct j2.post_id) jjim\n" +
+                "\n" +
+                "from post p\n" +
+                "         left join post_photos pp on p.post_id = pp.post_id\n" +
+                "         left outer join jjim j2 on p.post_id = j2.post_id\n" +
+                "         left join post_tags pt on p.post_id = pt.post_id\n" +
+                "\n" +
+                "where (p.status = 1 or p.status = 3) \n" +
+                "and (title LIKE CONCAT('%',?,'%') or title LIKE CONCAT('%',?,'%'))\n" +
+                "\n" +
+                "group by p.post_id";
+        Object[] getPostsParam = new Object[]{brandKor, brandEng};
+        return this.jdbcTemplate.query(getPostsQuery,
+                (rs, rowNum) -> new GetHomePostsRes(
+                        rs.getInt("post_id"),
+                        rs.getString("image_path"),
+                        rs.getInt("price"),
+                        rs.getString("title"),
+                        rs.getInt("safepay"),
+                        rs.getString("region"),
+                        rs.getString("created_at"),
+                        rs.getInt("jjim")
+                ), getPostsParam);
+    }
+
 
 }
